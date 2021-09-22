1ff3f91a1a8f59c0653646f0d5d310e368679a69
yairavivi4 <yavivi441@gmail.com>
2021-09-22 23:55:12 +0300

    finish navbar

--- src/components/layout/Navbar/NavBar.tsx
+++ src/components/layout/Navbar/NavBar.tsx
@@ -4,29 +4,27 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import './Navbar.css';
+import logo from '../../../images/GettFeldman.png';
 
 export default function Navbar() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
+        <Box sx={{flexGrow: 1}}>
             <AppBar position="static">
+
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
+                    <img src={logo} alt="Logo" className="logo"/>
+                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                        <Button color="inherit" size="large">Order</Button>
+                        <Button color="inherit" size="large">Time Table</Button>
+                        <Button color="inherit" size="large">Print</Button>
+                        <Button color="inherit" size="large">Manage</Button>
                     </Typography>
+
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
+
         </Box>
     );
 }
